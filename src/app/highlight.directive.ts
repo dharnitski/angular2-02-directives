7be49b2d3ab7096581ec58518b3a3cc45716f983
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-01 08:38:43 -0500

    add Directive property binding

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,15 +1,15 @@
-import { Directive, ElementRef, Renderer, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[dirHighlight]'
 })
 export class HighlightDirective {
   @HostListener('mouseenter') mouseover() {
-    this.backgroundColor = 'green';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave() {
-    this.backgroundColor = 'white';
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('click', ['$event'])
@@ -21,7 +21,10 @@ export class HighlightDirective {
     return this.backgroundColor;
   }
 
-  private backgroundColor = 'white';
+  @Input() defaultColor = 'white';
+  @Input('dirHighlight') highlightColor = 'green';
+
+  private backgroundColor: string;
 
   constructor(
     private elementRef: ElementRef,
@@ -29,6 +32,10 @@ export class HighlightDirective {
   ) {
     //this.elementRef.nativeElement.style.backgroundColor = 'green';
     this.renderer.setElementStyle(this.elementRef.nativeElement, 'border', '3px solid blue');
+  }
+
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor
   }
 
 }

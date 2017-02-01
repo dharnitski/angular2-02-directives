8a79769e3bdfa1bd20c84c02810427b1f51a1d07
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-01 08:11:45 -0500

    add HostListener and HostBinding to custom directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,15 +1,34 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[dirHighlight]'
 })
 export class HighlightDirective {
+  @HostListener('mouseenter') mouseover() {
+    this.backgroundColor = 'green';
+  }
+
+  @HostListener('mouseleave') mouseleave() {
+    this.backgroundColor = 'white';
+  }
+
+  @HostListener('click', ['$event'])
+  onClick(event) {
+    console.log("Event Target" + event.target);
+  }
+
+  @HostBinding('style.backgroundColor') get setColor() {
+    return this.backgroundColor;
+  }
+
+  private backgroundColor = 'white';
+
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer
   ) {
     //this.elementRef.nativeElement.style.backgroundColor = 'green';
-    this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
+    this.renderer.setElementStyle(this.elementRef.nativeElement, 'border', '3px solid blue');
   }
 
 }
